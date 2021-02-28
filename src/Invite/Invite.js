15e524101bd76517b06bf987a891358dbe797e84
Divya_Sampath <divya.s@8kmiles.com>
2021-02-28 16:53:42 +0530

    1st commit

---
+++ src/Invite/Invite.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as c from '../utils/config';
+import './Invite.css';
+import ring from '../utils/images/ring.jpg';
+import temple from '../utils/images/temple.jpg';
+import beach from '../utils/images/beach.jpg'
+
+class Invite extends React.Component{
+    render(){
+        return(
+            <>
+                <div className = 'msg-1'>
+              
+                <p className ='txt'>{c.msg.msg}</p>
+                
+                </div>
+                <div className = 'msg'>
+                    <div className = 'div-section'>
+                <p className ='txt'>{c.wedding.msg}</p>
+                <a href="http://maps.google.com/?q=1200 tirupati">{c.wedding.venue}</a>
+                </div>
+                <img src = {temple} alt = 'temple' className = 'img'/>
+                
+                </div>
+                <div className = 'msg'>
+                <img src = {beach} alt = 'beach' className = 'img'/>
+                    <div className = 'div-section'>
+                <p className ='txt'>{c.wedding.msg}</p>
+                <p className ='txt'>{c.wedding.venue}</p>
+                </div>
+                
+                </div>
+            </>
+        )
+    }
+}
+
+export default Invite;
---
+++ src/utils/config.js
@@ -0,0 +1,15 @@
+export const msg = {
+  msg: "What's meant to be,will find its way eventually. And YES, we found each other. We are getting married soon !! ",
+  img: "",
+};
+
+export const wedding = {
+  msg: "Join us and shower your blessings",
+  venue : 'At Sri Swamy Venktaeshwara temple, Tirumala Tirupati. Between ',
+  img: "",
+};
+
+export const recep = {
+  msg: "",
+  img: "",
+};
