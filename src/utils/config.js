13bc3bbe55eedb37a9edd4ae95e9cbf0f3ed1ea7
Divya_Sampath <divya.s@8kmiles.com>
2021-03-05 23:46:51 +0530

    changed themes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,15 +1,18 @@
 export const msg = {
+  title : "SOMU WEDS SHRADDHA !!",
   msg: "What's meant to be,will find its way eventually. And YES, we found each other. We are getting married soon !! ",
   img: "",
 };
 
 export const wedding = {
+  title : "Wedding",
   msg: "Join us and shower your blessings",
   venue : 'At Sri Swamy Venktaeshwara temple, Tirumala Tirupati. Between ',
   img: "",
 };
 
 export const recep = {
+  title : "Reception",
   msg: "",
   img: "",
 };
