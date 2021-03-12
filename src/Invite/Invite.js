8b203275e660c07d9660cc2633addb0b6f22319a
Divya_Sampath <divya.s@8kmiles.com>
2021-03-12 20:16:05 +0530

    changed bg images and colors

--- src/Invite/Invite.js
+++ src/Invite/Invite.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import * as c from '../utils/config';
-import './Invite.css';
-import temple from '../utils/images/wed2.jpg';
-import beach2 from '../utils/images/beach3.jpg'
+import React from "react";
+import * as c from "../utils/config";
+import "./Invite.css";
+import temple from "../utils/images/wed2.jpg";
+import beach2 from "../utils/images/beach4.jpg";
 
-class Invite extends React.Component{
-    render(){
-        return(
-            <>
-                <div className = 'msg-1'>
-               <p className = 'title'>{c.msg.title}</p>
-                <p className ='txt'>{c.msg.msg}</p>
-                
-                </div>
-                <div className = 'msg-2'>
-                    <div className = 'div-section1'>
-                <p className ='txt'>{c.wedding.msg}</p>
-                <a href="http://maps.google.com/?q=1200 tirupati">{c.wedding.venue}</a>
-                </div>
-                <img src = {temple} alt = 'temple' className = 'img'/>
-                
-                </div>
-                <div className = 'msg-3'>
-                <img src = {beach2} alt = 'beach' className = 'img'/>
-                    <div className = 'div-section2'>
-                <p className ='txt'>{c.wedding.msg}</p>
-                <p className ='txt'>{c.wedding.venue}</p>
-                </div>
-                
-                </div>
-            </>
-        )
-    }
+class Invite extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="msg-1">
+          <p className="title">{c.msg.title}</p>
+          <p className="txt">{c.msg.msg}</p>
+        </div>
+        <div className="msg-2">
+          <div className="div-section1">
+            <p className="txt">{c.wedding.msg}</p>
+            <a href="http://maps.google.com/?q=1200 tirupati">
+              {c.wedding.venue}
+            </a>
+          </div>
+          <img src={temple} alt="temple" className="img" />
+        </div>
+        <div className="msg-3">
+          <img src={beach2} alt="beach" className="img" />
+          <div className="div-section2">
+            <p className="txt">{c.wedding.msg}</p>
+            <p className="txt">{c.wedding.venue}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Invite;
